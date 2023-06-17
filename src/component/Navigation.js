f0147068d8de45e30a7d2be2c1ca364d10c3c19c
Nam An <nguynphmnaman@gmail.com>
2023-06-17 10:12:29 +0700

    exs13

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Navbar, NavItem, Icon } from 'react-materialize'
 export default function Navigation() {
     return (
-        <div>
-            <nav className='nav'>
-                <ul>
-                    <a className='active' href=' '>Home</a>
-                    <a className='active' href='#news'>News</a>
-                    <a className='active' href='#about'>About</a>
-                    <a className='active' href='#contact'>Contact</a>
-                </ul>
-            </nav>
-        </div>
+        <Navbar className="menu" 
+            alignLinks="right"
+            brand={<span className="brand-logo" >Player Cards</span>}
+            id="mobile-nav"
+            menuIcon={<Icon>menu</Icon>}>
+                
+            <ul>
+                <li> <Link to='/'><Icon left></Icon> Home</Link>
+                </li>
+                <li to='/about'><Link to='/about'> <Icon left></Icon> About</Link>
+                </li>
+                <li to='/news'> <Link to='/news'> <Icon left></Icon> News</Link>
+                </li>
+                <li href='/contact'><Link to='/contact'> <Icon left></Icon> Contact </Link>
+                </li>
+            </ul>
+        </Navbar>
     )
 }
